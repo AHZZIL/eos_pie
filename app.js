d4b79b05c27dd8aea5d84fb3f36691171d2a8570
jyoung <violentjy@gmail.com>
2016-02-08 20:07:46 +0900

    home 화면 장난질 ㅎㅎ

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/home', require('./routes/home'));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
